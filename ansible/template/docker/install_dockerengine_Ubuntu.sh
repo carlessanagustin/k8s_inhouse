bc6a06240b7e5380e67d53d31597cfe2aa857141
Carles San Agustin <carles.sam@gmail.com>
2019-01-08 10:59:53 +0100

    fixed docker engine version

---
+++ ansible/template/docker/install_dockerengine_Ubuntu.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# DOCKER_VERSION={{ docker_engine_version }}
+# USER={{ docker_user }}
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+sudo apt-get -y remove docker-ce
+sudo apt-get -y remove docker docker-engine docker.io
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+
+# sudo apt-cache madison docker-ce | awk '{print $3}'
+sudo apt-get -y install docker-ce=$DOCKER_VERSION
+
+sudo systemctl enable docker
+sudo systemctl restart docker || sudo service docker restart
+
+# echo manual | sudo tee /etc/init/docker.override
+# sudo update-rc.d docker defaults
